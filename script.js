f22155211f2109be82e6777923c6d78d34e5e39c
Hemanth-Sappa <134211683+Hemanth-Sappa@users.noreply.github.com>
2023-07-19 19:45:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 let p = document.createElement("p");
 const date = new Date();
-p.appendChild(date);
+const node = document.createTextNode(date);
+p.appendChild(node);
 document.body.appendChild(p);
 
